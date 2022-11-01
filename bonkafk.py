2145f3a8593127ea04c779aaff53f8f975a3c60d
den-dover <86752206+den-dover@users.noreply.github.com>
2022-11-01 17:17:31 -0400

    Update bonkafk.py

--- bonkafk.py
+++ bonkafk.py
@@ -1,6 +1,12 @@
-from telnetlib import GA
-import time, 
+import time
 import pyautogui as gui
+from datetime import datetime
+import discord
+from PIL import ImageGrab
+
+
+Stopping = 'false'
+global stopping
 
 global Loop
 global TeamChange
@@ -30,22 +36,28 @@ Game2ForthTeam = 'undefined'
 CurrentGameHost = 'undefined'
 DefinedHostTab = 'true'
 CurrentGame = 'undefined'
+placeholder = 'default'
+count = 0
 gui.PAUSE = 0.000001
+    
 Game1FirstTeam = input('what is the current Game 1 host tab team? ')
 time.sleep(0.2)
 Game2FirstTeam = input('what is the current Game 2 host tab team? ')
 time.sleep(0.2)
-Monitor = input('what is the current monitor, e.g., laptop, left hp, main dell, old tvs ')
+Monitor = input('what is the current monitor, e.g., laptop, left hp, main dell, old tvs (remember if you are on old tvs hit the down arrow 3 times) ')
 time.sleep(0.2)
 WatchGame = input('what game would you like to watch? e.g., 1, 2 ')
-
+time.sleep(0.2)
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M")
+print('afk started at time-- ', dt_string)
 def StartGameButton():
     if Monitor == 'left hp':
         gui.leftClick(-686, 1383)
     elif Monitor == 'laptop':
         gui.leftClick(1302, 902)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(1331, 892)
 
 def RedJoin():
     if Monitor == 'left hp':
@@ -53,7 +65,7 @@ def RedJoin():
     elif Monitor == 'laptop':
         gui.leftClick(711, 554)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(696, 521)
 
 def BlueJoin():
     if Monitor == 'left hp':
@@ -61,7 +73,7 @@ def BlueJoin():
     elif Monitor == 'laptop':
         gui.leftClick(568, 560)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(531, 524)
 
 def GreenJoin():
     if Monitor == 'left hp':
@@ -69,7 +81,7 @@ def GreenJoin():
     elif Monitor == 'laptop':
         gui.leftClick(866, 555)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(860, 518)
 
 def YellowJoin():
     if Monitor == 'left hp':
@@ -77,7 +89,7 @@ def YellowJoin():
     elif Monitor == 'laptop':
         gui.leftClick(1006, 553)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(1010, 519)
 
 def FirstTab():
     if Monitor == 'left hp':
@@ -85,7 +97,7 @@ def FirstTab():
     elif Monitor == 'laptop':
         gui.leftClick(104, 21)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(117, 15)
 
 def SecondTab():
     if Monitor == 'left hp':
@@ -93,7 +105,7 @@ def SecondTab():
     elif Monitor == 'laptop':
         gui.leftClick(318, 23)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(301, 21)
 
 def ThirdTab():
     if Monitor == 'left hp':
@@ -101,7 +113,7 @@ def ThirdTab():
     elif Monitor == 'laptop':
         gui.leftClick(515, 26)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(499, 36)
 
 def ForthTab():
     if Monitor == 'left hp':
@@ -109,7 +121,7 @@ def ForthTab():
     elif Monitor == 'laptop':
         gui.leftClick(692, 23)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(668, 33)
 
 def FifthTab():
     if Monitor == 'left hp':
@@ -117,7 +129,7 @@ def FifthTab():
     elif Monitor == 'laptop':
         gui.leftClick(871, 26)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(849, 22)
 
 def SixthTab():
     if Monitor == 'left hp':
@@ -125,7 +137,7 @@ def SixthTab():
     elif Monitor == 'laptop':
         gui.leftClick(1047, 12)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(1063, 25)
 
 def SeventhTab():
     if Monitor == 'left hp':
@@ -133,7 +145,7 @@ def SeventhTab():
     elif Monitor == 'laptop':
         gui.leftClick(1256, 19)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(1220, 30)
 
 def EighthTab():
     if Monitor == 'left hp':
@@ -141,7 +153,7 @@ def EighthTab():
     elif Monitor == 'laptop':
         gui.leftClick(1480, 34)
     elif Monitor == 'old tvs':
-        gui.leftClick()
+        gui.leftClick(1412, 26)
 
 def MapFav():
     if Monitor == 'left hp':
@@ -149,42 +161,8 @@ def MapFav():
     elif Monitor == 'laptop':
         gui.leftClick(951, 758)
         favasdasd = 0
-
-def HostTab():
-    global Loop
-    global TeamChange
-    global Game1FirstTeam
-    global Game1SecondTeam
-    global Game1ThirdTeam
-    global Game1ForthTeam
-    global Game2FirstTeam
-    global Game2SecondTeam
-    global Game2ThirdTeam
-    global Game2ForthTeam
-    global CurrentGameHost
-    global DefinedHostTab
-    if CurrentGame == 1:
-        CurrentGameHost = Game1HostTeam
-    else:
-        CurrentGameHost = Game2HostTeam
-    if CurrentGameHost == 1:
-        FirstTab()
-    elif CurrentGameHost == 2:
-        SecondTab()
-    elif CurrentGameHost == 3:
-        ThirdTab()
-    elif CurrentGameHost == 4:
-        ForthTab()
-    elif CurrentGameHost == 5:
-        FifthTab()
-    elif CurrentGameHost == 6:
-        SixthTab()
-    elif CurrentGameHost == 7:
-        SeventhTab()
-    elif CurrentGameHost == 8:
-        EighthTab()
-    else:
-        DefinedHostTab = 'false'
+    elif Monitor == 'old tvs':
+        gui.leftClick(946, 738)
 
 def FirstGame():
     global Loop
@@ -438,22 +416,11 @@ def SecondGame():
         time.sleep(0.3)
         BlueJoin()
         Game2ForthTeam = 'blue'
-
-def GameStart():
-    time.sleep(1)
-    HostTab()
-    if DefinedHostTab == 'true':
-        time.sleep(0.5)
-        StartGameButton()
-        time.sleep(0.3)
-    else: 
-        print("no game host tab defined, couldn't start game")
     
 time.sleep(3)
-while True:
-    xpCooldownBypass = xpCooldownBypass + 1
-    if xpCooldownBypass == 2:
-        print('stopping temperarily in attempt to bypass xp cooldown')
+while Loop < 1000:
+    if placeholder == 'asdasddsasaddas':
+        placeholder = placeholder
     else:
         time.sleep(0.2)
         CurrentGame = 1
@@ -506,4 +473,11 @@ while True:
                             print('eighth tab was blue')
                         else:
                             print('no tab with team blue found')
-    time.sleep(41)
+    time.sleep(28)
+    myScreenshot = ImageGrab.grab(bbox=(458, 252, 1100, 503))
+    myScreenshot.save(r'C:/Users/clari/Documents/code/info1.png')
+    time.sleep(0.25)
+    FirstTab()
+    time.sleep(0.25)
+    myScreenshot2 = ImageGrab.grab(bbox=(458, 252, 1100, 503))
+    myScreenshot2.save(r'C:/Users/clari/Documents/code/info2.png')
